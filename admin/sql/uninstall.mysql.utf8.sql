5af0d49585124f13c32d8272b8db0096c992eddb
Rob Poort <robpoort@gmail.com>
2012-11-06 14:13:11 +0100

    uninstall file

---
+++ admin/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `#__jexbooking_location`;
+
+DROP TABLE IF EXISTS `#__jexbooking_type`;
+
+DROP TABLE IF EXISTS `#__jexbooking_attributes`;
+
+DROP TABLE IF EXISTS `#__jexbooking_default_prices`;
+
+DROP TABLE IF EXISTS `#__jexbooking_arrangements`;
+
+DROP TABLE IF EXISTS `#__jexbooking_xref_attributes`;
