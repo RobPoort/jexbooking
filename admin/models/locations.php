09b7790ff12216f3d9fbb12907b14dde52d3cb9f
Rob Poort <robpoort@gmail.com>
2012-11-19 16:09:39 +0100

    taalbestand

---
+++ admin/models/locations.php
@@ -0,0 +1,53 @@
+<?php
+defined('_JEXEC') or die('Restricted Access');
+
+jimport('joomla.application.component.modellist');
+
+class JexBookingModelLocations extends JModelList
+{
+	public function getListQuery(){
+		
+		$db = JFactory::getDBO();
+		$query = $db->getQuery(true);
+		$query->from('#__jexbooking_location as jl');
+		$query->select('jl.id as id, jl.name as name');
+		
+		return $query;
+	}
+	function publish(){
+	
+		$data = JRequest::get('post');
+		$value = JRequest::getCmd('task');
+		$ids = JRequest::getVar('cid', array(), 'post', 'array');
+		$where = array();
+		foreach($ids as $id){
+			$where[] = ' id='.(int)$id;
+		}
+		switch($value){
+			case 'publish':
+				$value = 1;
+				break;
+			case 'unpublish';
+				$value = 0;
+				break;
+			default:
+				$value = 1;
+				break;
+		}
+				
+		$db = JFactory::getDBO();
+		$query = $db->getQuery(true);
+		$query->clear();
+		$query->update('#__jexbooking_location');
+		$query->set('published = '.(int)$value);
+		$query->where($where, ' OR ');
+		$db->setQuery((string)$query);
+		if (!$db->query()) {
+            JError::raiseError(500, $db->getErrorMsg());
+        	return false;
+        } else {
+        	return true;
+		}	
+		 
+	}
+}
